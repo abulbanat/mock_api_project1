97244b2cce46f34015c748213f0f8ecb50b52c1b
abulbanat <umarovmuhammad28@gmail.com>
2023-11-24 11:40:17 +0500

    Muhammad, 2-commit, task 3 added

--- bin/hometask1.dart
+++ bin/hometask1.dart
@@ -1,7 +1,17 @@
+/*
 import 'services/networking_service.dart';
 
 void main() async{
   String respons1 = await DATA.getData1(DATA.apiArt);
   print(respons1);
-  
+
 }
+*/
+import 'services/networking_service.dart';
+
+void main() async{
+  String respons2 = await DATA1.getData2(DATA1.apiProduct);
+  print(respons2);
+
+}
+
--- bin/services/networking_service.dart
+++ bin/services/networking_service.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:http/http.dart';
 
 class DATA{
@@ -9,7 +10,16 @@ class DATA{
     Response response = await get(url);
     return response.body;
   }
+}*/
+import 'package:http/http.dart';
 
+class DATA1 {
+  static final String baseUrl2 = "655deeb89f1e1093c59a2ed1.mockapi.io";
+  static final String apiProduct = "/product";
 
-
+  static Future<String> getData2(String api) async {
+    Uri url = Uri.https(baseUrl2, api);
+    Response response = await get(url);
+    return response.body;
+  }
 }
